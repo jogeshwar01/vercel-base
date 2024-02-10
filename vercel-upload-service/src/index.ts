fa11b0850e8b6c4084cf3261397c45f6126ff439
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2024-02-10 17:48:22 +0530

    feat: get list of all files in a folder

--- vercel-upload-service/src/index.ts
+++ vercel-upload-service/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors'
 import path from 'path'
 import { generate } from './utils'
+import { getAllFiles } from './file';
 import simpleGit from 'simple-git';
 import dotenv from 'dotenv';
 dotenv.config()
@@ -16,6 +17,9 @@ app.post('/upload', async (req: Request,res: Response) => {
     const id = generate()
 
     await simpleGit().clone(req.body.repoUrl, path.join(__dirname,`/output/${id}`))
+
+    const files = getAllFiles(path.join(__dirname,`/output/${id}`))
+    console.log(files)
 
     res.json({
         id : id
