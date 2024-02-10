5ee3646f1212c96159e17cc2721abc51d24c9cd2
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2024-02-10 17:48:52 +0530

    fix: format using es-lint prettier

--- vercel-upload-service/src/index.ts
+++ vercel-upload-service/src/index.ts
@@ -1,31 +1,34 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors'
-import path from 'path'
-import { generate } from './utils'
-import { getAllFiles } from './file';
-import simpleGit from 'simple-git';
-import dotenv from 'dotenv';
-dotenv.config()
+import express, { Request, Response } from "express";
+import cors from "cors";
+import path from "path";
+import { generate } from "./utils";
+import { getAllFiles } from "./file";
+import simpleGit from "simple-git";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 const port = process.env.UPLOAD_SERVICE_PORT;
 
-app.post('/upload', async (req: Request,res: Response) => {
-    const id = generate()
+app.post("/upload", async (req: Request, res: Response) => {
+    const id = generate();
 
-    await simpleGit().clone(req.body.repoUrl, path.join(__dirname,`/output/${id}`))
+    await simpleGit().clone(
+        req.body.repoUrl,
+        path.join(__dirname, `/output/${id}`)
+    );
 
-    const files = getAllFiles(path.join(__dirname,`/output/${id}`))
-    console.log(files)
+    const files = getAllFiles(path.join(__dirname, `/output/${id}`));
+    console.log(files);
 
     res.json({
-        id : id
-    })
-})
+        id: id,
+    });
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+    console.log(`Listening on port ${port}`);
+});
--- vercel-upload-service/src/utils.ts
+++ vercel-upload-service/src/utils.ts
@@ -1,10 +1,9 @@
-
-export function generate(){
+export function generate() {
     const subset = "123456789qwertyuiopasdfghjklzxcvbnm";
-	const length = 5;
-	let id = "";
-	for (let i = 0; i < length; i++) {
-		id += subset[Math.floor(Math.random() * subset.length)];
-	}
-	return id;
+    const length = 5;
+    let id = "";
+    for (let i = 0; i < length; i++) {
+        id += subset[Math.floor(Math.random() * subset.length)];
+    }
+    return id;
 }
