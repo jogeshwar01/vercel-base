61d0d959df8bb3d57676cbfa0c84ede3ce989d3f
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2024-02-10 17:27:21 +0530

    feat: clone git repo using simple-git

--- vercel-upload-service/src/index.ts
+++ vercel-upload-service/src/index.ts
@@ -1,5 +1,8 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors'
+import path from 'path'
+import { generate } from './utils'
+import simpleGit from 'simple-git';
 import dotenv from 'dotenv';
 dotenv.config()
 
@@ -8,6 +11,16 @@ app.use(cors())
 app.use(express.json())
 
 const port = process.env.UPLOAD_SERVICE_PORT;
+
+app.post('/upload', async (req: Request,res: Response) => {
+    const id = generate()
+
+    await simpleGit().clone(req.body.repoUrl, path.join(__dirname,`/output/${id}`))
+
+    res.json({
+        id : id
+    })
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
