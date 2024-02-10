25526f95a9c78865ab5284e9132c06e3aed6fdf2
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2024-02-10 18:46:43 +0530

    feat: upload git repo dir to s3

---
+++ vercel-upload-service/src/aws.ts
@@ -0,0 +1,25 @@
+import { AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_S3_BUCKET } from "./config";
+import { S3 } from "aws-sdk";
+import fs from "fs";
+
+const s3 = new S3({
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_SECRET_KEY,
+});
+
+// fileName => output/12312/src/App.jsx
+// filePath => /home/jogeshwar/vercel/dist/output/12312/src/App.jsx
+export const uploadFile = async (fileName: string, localFilePath: string) => {
+    const fileContent = fs.readFileSync(localFilePath);
+    const s3Bucket = AWS_S3_BUCKET ?? "vercel-data-store";
+
+    const response = await s3
+        .upload({
+            Body: fileContent,
+            Bucket: s3Bucket,
+            Key: fileName,
+        })
+        .promise();
+
+    console.log(response);
+};
--- vercel-upload-service/src/index.ts
+++ vercel-upload-service/src/index.ts
@@ -1,17 +1,15 @@
+import { UPLOAD_SERVICE_PORT } from "./config";
 import express, { Request, Response } from "express";
 import cors from "cors";
 import path from "path";
 import { generate } from "./utils";
 import { getAllFiles } from "./file";
 import simpleGit from "simple-git";
-import dotenv from "dotenv";
-dotenv.config();
+import { uploadFile } from "./aws";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-const port = process.env.UPLOAD_SERVICE_PORT;
 
 app.post("/upload", async (req: Request, res: Response) => {
     const id = generate();
@@ -22,13 +20,16 @@ app.post("/upload", async (req: Request, res: Response) => {
     );
 
     const files = getAllFiles(path.join(__dirname, `/output/${id}`));
-    console.log(files);
+
+    files.forEach(async (file) => {
+        await uploadFile(file.slice(__dirname.length + 1), file);
+    });
 
     res.json({
         id: id,
     });
 });
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+app.listen(UPLOAD_SERVICE_PORT, () => {
+    console.log(`Listening on port ${UPLOAD_SERVICE_PORT}`);
 });
